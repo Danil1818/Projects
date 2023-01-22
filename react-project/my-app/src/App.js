8b8a0c84bff73acc000091d2c7055138b7984ebf
Danil1818 <daniktod12@gmail.com>
2023-01-23 00:25:13 +0200

    created your own hooks (usePosts)

--- react-project/my-app/src/App.js
+++ react-project/my-app/src/App.js
@@ -1,37 +1,19 @@
-import React, { useMemo, useState } from 'react'
+import React, { useState } from 'react'
 import PostFilter from './components/PostFilter'
 import PostForm from './components/PostForm'
 import PostList from './components/PostList'
 import MyButton from './components/UI/button/MyButton'
 import MyModal from './components/UI/modal/MyModal'
 import './styles/App.css'
+import { usePosts } from './hooks/usePosts'
 
 // rfce - reactFunctionalComponentExport
 
 function App() {
-	const [posts, setPosts] = useState([
-		{ id: 1, title: 'HTML', body: 'HyperText Markup Language' },
-		{ id: 2, title: 'CSS', body: 'This is Cascading Style Sheets' },
-		{ id: 3, title: 'JavaScript', body: 'Programming Language' },
-	])
-
+	const [posts, setPosts] = useState([])
 	const [filter, setFilter] = useState({ sort: '', query: '' })
 	const [modal, setModal] = useState(false)
-
-	const sortedPosts = useMemo(() => {
-		if (filter.sort) {
-			return [...posts].sort((a, b) =>
-				a[filter.sort].localeCompare(b[filter.sort])
-			)
-		}
-		return posts
-	}, [filter.sort, posts])
-
-	const sortedAndSelectPosts = useMemo(() => {
-		return sortedPosts.filter(post =>
-			post.title.toLowerCase().includes(filter.query.toLowerCase())
-		)
-	}, [filter.query, sortedPosts])
+	const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
 
 	const createPost = newPost => {
 		setPosts([...posts, newPost])
@@ -42,14 +24,12 @@ function App() {
 	const removePost = post => {
 		setPosts(posts.filter(p => p.id !== post.id))
 	}
-
-	console.log('Ok, its working@!!')
-
+ 
 	return (
 		<div className='App'>
-		<MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-			Create user
-		</MyButton>
+			<MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+				Create user
+			</MyButton>
 			<MyModal visible={modal} setVisible={setModal}>
 				<PostForm create={createPost} />
 			</MyModal>
@@ -57,7 +37,7 @@ function App() {
 			<PostFilter filter={filter} setFilter={setFilter} />
 			<PostList
 				remove={removePost}
-				posts={sortedAndSelectPosts}
+				posts={sortedAndSearchPosts }
 				title='Posts'
 			/>
 		</div>
